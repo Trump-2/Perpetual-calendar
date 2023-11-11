d4a9cc3ec49b4ef72f3e920a15a387f054a1735e
Trump-2 <akashidaiki2@gmail.com>
2023-11-11 23:17:44 +0800

    調整 form 表單位置

--- index.php
+++ index.php
@@ -54,21 +54,21 @@ switch ($month) {
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
-    integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="shortcut icon" href="#">
-  <link rel="stylesheet" href="style.css">
-  <title>萬年曆作業</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="shortcut icon" href="#">
+    <link rel="stylesheet" href="style.css">
+    <title>萬年曆作業</title>
 
 </head>
 
 <body style="background-image:url('<?php echo $Bg ?>')">
 
-  <?php
+    <?php
   // 名言的array
   $sayingArr = ['From error to error one discovers the entire truth.', 'The only impossible journey is the one you never begin.', 'Strength and growth come only through continuous effort and struggle.', 'However difficult life may seem, there is always something you can do and succeed at.', 'The beautiful thing about learning is nobody can take it away from you.', 'I am always doing that which I cannot do, in order that I may learn how to do it.', 'There is no such thing as a great talent without great will - power.', 'I don\'t wait for moods. You accomplish nothing if you do that. Your mind must know it has got down to work.', 'You can overcome anything, if and only if you love something enough.', 'Talent without working hard is nothing.', 'Find a group of people who challenge and inspire you; spend a lot of time with them, and it will change your life.', 'Life is like a game of cards. The hand you are dealt is determinism; the way you play it is free will.'];
 
@@ -108,55 +108,56 @@ switch ($month) {
   ?>
 
 
-  <div class="big-container">
+    <div class="big-container">
 
-    <div class="container">
-      <div class=header>
-        <div class="title"><span>P</span>erpetual<span> c</span>alendar</div>
+        <div class="container">
+            <div class=header>
+                <div class="title"><span>P</span>erpetual<span> c</span>alendar</div>
 
-        <div class="none"></div>
-        <div class="return"> <a href='?year=<?= date('Y') ?>&month=<?= date('n') ?>'><i
-              class="fa-solid fa-rotate-right"></i></a>
-        </div>
-        <div class="clock">
-          <span id="hours"></span>
-          <span>:</span>
-          <span id="minutes"></span>
-          <span>:</span>
-          <span id="seconds"></span>
-        </div>
-      </div>
-      <div class="box">
-        <a class="prev" href='?year=<?= $prevYear ?>&month=<?= $prev ?>'><i class="fa-regular fa-circle-left"></i></a>
-        <div class='aside-div'>
-          <aside>
-          </aside>
-        </div>
-        <div class="main-div">
-          <main>
-            <div class=month-year>
-              <div class="month">
-                <?php
+                <div class="none"></div>
+                <div class="return"> <a href='?year=<?= date('Y') ?>&month=<?= date('n') ?>'><i
+                            class="fa-solid fa-rotate-right"></i></a>
+                </div>
+                <div class="clock">
+                    <span id="hours"></span>
+                    <span>:</span>
+                    <span id="minutes"></span>
+                    <span>:</span>
+                    <span id="seconds"></span>
+                </div>
+            </div>
+            <div class="box">
+                <a class="prev" href='?year=<?= $prevYear ?>&month=<?= $prev ?>'><i
+                        class="fa-regular fa-circle-left"></i></a>
+                <div class='aside-div'>
+                    <aside>
+                    </aside>
+                </div>
+                <div class="main-div">
+                    <main>
+                        <div class=month-year>
+                            <div class="month">
+                                <?php
               echo date('M', strtotime(date("Y-$month-1")));
               ?>
-              </div>
-              <div class="year">
-                <?php
+                            </div>
+                            <div class="year">
+                                <?php
               echo $year;
               ?>
-              </div>
-            </div>
-            <table>
-              <tr>
-                <th>Sun.</th>
-                <th>Mon.</th>
-                <th>Tue.</th>
-                <th>Wed.</th>
-                <th>Thu.</th>
-                <th>Fri.</th>
-                <th>Sat.</th>
-              </tr>
-              <?php
+                            </div>
+                        </div>
+                        <table>
+                            <tr>
+                                <th>Sun.</th>
+                                <th>Mon.</th>
+                                <th>Tue.</th>
+                                <th>Wed.</th>
+                                <th>Thu.</th>
+                                <th>Fri.</th>
+                                <th>Sat.</th>
+                            </tr>
+                            <?php
             // $i 影響週數
             for ($i = 0; $i < $weeks; $i++) {
               echo "<tr>";
@@ -198,57 +199,59 @@ switch ($month) {
             echo "</div>";
             echo "</div>";
             ?>
-          </main>
+                    </main>
 
+                </div>
+                <a class="next" href='?year=<?= $nextYear ?>&month=<?= $next ?>'><i
+                        class="fa-regular fa-circle-left fa-flip-horizontal"></i></a>
+            </div>
         </div>
-        <a class="next" href='?year=<?= $nextYear ?>&month=<?= $next ?>'><i
-            class="fa-regular fa-circle-left fa-flip-horizontal"></i></a>
-      </div>
+        <div class="container2"></div>
+        <div class="container3">
+            <form class="m-1 text-center form-inline " action="index.php" method="get">
+                <div>Search</div>
+                <input class=" form-control " type="text" name="year" placeholder="Year" required="">
+                <select name="month" class="btn btn-light maL ">
+                    <option value="1">January</option>
+                    <option value="2">February</option>
+                    <option value="3">March</option>
+                    <option value="4">April</option>
+                    <option value="5">May</option>
+                    <option value="6">June</option>
+                    <option value="7">July</option>
+                    <option value="8">August</option>
+                    <option value="9">September</option>
+                    <option value="10">October</option>
+                    <option value="11">November</option>
+                    <option value="12">December</option>
+                </select>
+                <input type="submit" value="Go !">
+            </form>
+        </div>
+
     </div>
-    <div class="container">
-      <form class="m-1 text-center form-inline " action="index.php" method="get">
-        <input class=" form-control " type="text" name="year" placeholder="Year" required="">
-        <select name="month" class="btn btn-light maL ">
-          <option value="1">January</option>
-          <option value="2">February</option>
-          <option value="3">March</option>
-          <option value="4">April</option>
-          <option value="5">May</option>
-          <option value="6">June</option>
-          <option value="7">July</option>
-          <option value="8">August</option>
-          <option value="9">September</option>
-          <option value="10">October</option>
-          <option value="11">November</option>
-          <option value="12">December</option>
-        </select>
-        <input type="submit" value="Search">
-      </form>
-    </div>
 
-  </div>
+    <script>
+    function updateClock() {
+        var now = new Date();
+        var hours = now.getHours().toString().padStart(2, '0');
+        var minutes = now.getMinutes().toString().padStart(2, '0');
+        var seconds = now.getSeconds().toString().padStart(2, '0');
+        // var timeString = hours + ':' + minutes + ':' + seconds;
 
-  <script>
-  function updateClock() {
-    var now = new Date();
-    var hours = now.getHours().toString().padStart(2, '0');
-    var minutes = now.getMinutes().toString().padStart(2, '0');
-    var seconds = now.getSeconds().toString().padStart(2, '0');
-    // var timeString = hours + ':' + minutes + ':' + seconds;
+        // document.getElementById('clock').textContent = timeString;
+        document.getElementById('hours').textContent = hours;
+        document.getElementById('minutes').textContent = minutes;
+        document.getElementById('seconds').textContent = seconds;
 
-    // document.getElementById('clock').textContent = timeString;
-    document.getElementById('hours').textContent = hours;
-    document.getElementById('minutes').textContent = minutes;
-    document.getElementById('seconds').textContent = seconds;
+    }
 
-  }
+    // 初次載入頁面時執行
+    updateClock();
 
-  // 初次載入頁面時執行
-  updateClock();
-
-  // 每秒更新一次
-  setInterval(updateClock, 1000);
-  </script>
+    // 每秒更新一次
+    setInterval(updateClock, 1000);
+    </script>
 
 
 </body>
